ab8f2a39e2ff713d2cd70695d3fd03488cc6cf3e
ARTEMONSB <58790695+ARTEMONSB@users.noreply.github.com>
2022-05-06 14:43:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+import Web3 from 'web3';
+const web3 = new Web3('https://mainnet.infura.io/v3/5cfd4082f78246dba7b89998f2666dd8');
 
 const ethereumButton = document.getElementById("ttt");
 
 ethereumButton.addEventListener('click', () => {
     console.log("Hello");
 });
+
+web3.eth.getBlockNumber(function(error, result) {
+    console.log(result);
+})
